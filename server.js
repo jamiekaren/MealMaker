8d11848732e1566f47ef092884b9f0bdfadc18f2
Jasmin <jasymatos@gmail.com>
2019-09-14 11:38:32 -0400

    merging

--- server.js
+++ server.js
@@ -1,6 +1,36 @@
-// =============================================================
-var express = require("express");
+const express = require("express");
+const Sequelize = require("sequelize");
+const path = require("path");
+const fs = require("fs");
+const router = require("router");
+const db = require("./models")
+
+//EXPRESS CONFIGURATION
+const app = express();
+db.sequelize.sync()
+
+app.get("/", (req, res) => res.send("index"));
+
+const PORT = process.env.PORT || 3000;
+
+// app.listen(PORT, console.log("Server started on port"));
+
+// const db = require(".app/models/post.js");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
-var app = express();
+app.use(express.static("public"));
 
+const routes1 = require("./app/routes/apiroutes.js");
+const routes2 = require("./app/routes/htmlRoutes.js");
+// app.use(routes1)
+// app.use(routes2)
+
+
+// db.sequelize.sync({ force: true }).then(function() {
+    app.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+    });
+//   });
