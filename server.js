7c11473746be10846adb1e400cbcc3fd7dd2e4c4
William D. Yurkow <bcwill42@gmail.com>
2019-09-14 11:23:35 -0400

    working on the api routes.

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const router = require("router");
 //EXPRESS CONFIGURATION
 const app = express();
 
-app.get("/", (req, res) => res.send("index"));
+// app.get("/", (req, res) => res.send("index"));
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, console.log("Server started on port"));
+// app.listen(PORT, console.log("Server started on port"));
 
 // const db = require(".app/models/post.js");
 
@@ -21,10 +21,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-const routes1 = require("./app/routes/apiroutes.js");
-const routes2 = require("./app/routes/htmlRoutes.js");
-app.use(routes1)
-app.use(routes2)
+// const routes1 = require("./app/routes/apiroutes.js");
+// const routes2 = require("./app/routes/htmlRoutes.js");
+// app.use(routes1)
+// app.use(routes2)
 
 
 // db.sequelize.sync({ force: true }).then(function() {
