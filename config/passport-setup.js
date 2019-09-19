48cfaef3c261a7ddf07422cb1e16fb4992275ebe
Jamie Ferrugiaro <missjamiekaren@gmail.com>
2019-09-19 12:55:26 -0400

    Added in auth routes, changed passport setup js

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,7 +1,56 @@
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20');
-// const keys = require('./keys');
-require('dotenv').config();
+// const GoogleStrategy = require('passport-google-oauth20');
+const LocalStrategy = require('passport-local').Strategy;
+//We will need the models to check passport gainst
+const db = require("../models")
+
+// require('dotenv').config();
+
+
+//This tells passport we want to use a Local Strategy (ie username/email & password)
+passport.use(new LocalStrategy(
+    {
+        usernameField: "email"
+    },
+    (email, password, done) => {
+        // when the user tries to sign in run this code
+        db.User.findOne({
+            where: {
+                email: email
+            }
+        }).then((user) => {
+            if (!user) {
+                return done(null, false, {
+                    message: "Incorrect email"
+                });
+            }
+            else if (!user.validPassword(password)) {
+                return done(null, false, {
+                    message: "Incorrect password."
+                });
+            }
+            return done(null, user);
+        });
+    }
+));
+
+
+// In order to help keep authentication state across HTTP requests,
+// Sequelize needs to serialize and deserialize the user
+// Just consider this part boilerplate needed to make it all work
+passport.serializeUser(function (user, cb) {
+    cb(null, user);
+});
+//
+passport.deserializeUser(function (obj, cb) {
+    cb(null, obj);
+});
+
+
+// Exporting our configured passport
+module.exports = passport;
+
+
 
 // passport.use(
 //     new GoogleStrategy({
@@ -16,14 +65,3 @@ require('dotenv').config();
 //         console.log(profile);
 //     })
 // );
-
-passport.use(new LocalStrategy(
-    function (username, password, done) {
-        User.findOne({ username: username }, function (err, user) {
-            if (err) { return done(err); }
-            if (!user) { return done(null, false); }
-            if (!user.verifyPassword(password)) { return done(null, false); }
-            return done(null, user);
-        });
-    }
-));
